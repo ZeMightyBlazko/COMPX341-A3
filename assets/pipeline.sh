5bb30dcb9b5abef1057d888cf8f6311e086bf521
Veela <veela@swipedon.com>
2022-05-15 09:51:41 +1200

    COMPX341-22A-A3 Adding custom commit messages to the pipeline

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,14 +1,26 @@
+# Written by Veela Moxham-Bettridge, ID:1537715 for COMPX341 Assignment 3 10/05/2022
+
 #!/bin/bash
+
+# NOTE: this script should be ran from within the assets folder (i.e. root/assets) as this code contains a relative path (used for 
+# actions related to Git) and there would be major consequences if not ran from assets (i.e. the pipeline code would break).
 
 #This function is for the adding, committing and pushing of code to GitHub (from root level)
 function git_commands () {
 	echo "Adding files to git..."
 	git add .
 	echo "Committing files to git..."
-	git commit -m "COMPX341-22A-A3 Committing from CI/CD Pipeline"
+	git commit -m "$1"
 	echo "Pushing files to git..."
 	git push
 }
+
+
+#check for a commit message, if there is no message, display a usage statement and exit the script
+if [ -z "$1" ] ; then
+	echo "Usage: pipeline.sh <commit message>"
+	exit 0
+fi
 
 
 #run the npm commands, if anything fails, an error message will be printed
@@ -17,7 +29,7 @@ if npm install ; then
 	echo "Running npm run build..."
 	if npm run build ; then
 		cd ../
-		git_commands
+		git_commands "$1"
 		cd assets/
 		echo "Running npm run start..."
 		if npm run start ; then
@@ -31,3 +43,6 @@ if npm install ; then
 else
 	echo "ERROR: npm install F A I L E D"
 fi
+
+
+
